7ec499dac8a48c3e5c23dfd617c8f36e01be3071
Fernando Araujo <36943054+fernandomaraujo@users.noreply.github.com>
2019-10-10 13:55:29 -0300

    Exclusão de variáveis não utilizadas

--- Lista 5/E03.c
+++ Lista 5/E03.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-/* 	QUEST√O 03: 
+/* 	QUEST√ÉO 03: 
 	Implementar um programa
 	que calcule o valor de base expoente, onde
-	base e expoente s„o n˙meros inteiros
-	dados pelo usu·rio. */
+	base e expoente s√£o n√∫meros inteiros
+	dados pelo usu√°rio. */
 
 int main() {
 
 	/*
 		2(4) = 2 x 2 x 2 x 2 = 16
-		2 È a base, 4 È o expoente, e 16 È a potÍncia
+		2 √© a base, 4 √© o expoente, e 16 √© a pot√™ncia
 	*/
 
-	int i, base, exp, res=0, aux=1, pot=1;
+	int i, base, exp, pot=1;
 	
 	printf("Informe valor da base: ");
 	scanf("%d", &base);
@@ -21,22 +21,22 @@ int main() {
 	printf("Informe valor do expoente: ");
 	scanf("%d", &exp);
 	
-	// se expoente igual a 0, a potencia È igual 1
+	// se expoente igual a 0, a potencia √© igual 1
 	if(exp == 0) {
 		
-		// valor inicial da potencia È 1, ent„o basta exibir pot
+		// valor inicial da potencia √© 1, ent√£o basta exibir pot
 		printf("\nPotencia: %d", pot);
 		
-		// caso n„o
+		// caso n√£o
 	} else {
 		
 		/*
-			faÁa
-				a potencia guardar o resultado da multiplicaÁ„o
+			fa√ßa
+				a potencia guardar o resultado da multiplica√ß√£o
 				consecutivas pela base
 			
-				e decrementar (atÈ 0) o valor do expoente, fazendo assim 
-				a repetiÁ„o executar tantas vezes o seu valor
+				e decrementar (at√© 0) o valor do expoente, fazendo assim 
+				a repeti√ß√£o executar tantas vezes o seu valor
 		
 			enquanto (valor do expoente for maior que 0)
 		*/
